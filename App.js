25b64c32f2951f547f7965a4ab6a09f1fbfd5c19
Ahmad D. Y. Nugroho <ahmaddwi700@gmail.com>
2021-12-28 01:31:39 +0700

    feat: notifications

--- App.js
+++ App.js
@@ -1,30 +1,123 @@
-import React, { useState } from 'react'
-import { Button, ScrollView, StyleSheet, Text, View } from 'react-native'
+import Constants from 'expo-constants'
+import * as Notifications from 'expo-notifications'
+import React, { useEffect, useRef, useState } from 'react'
+import { Button, Platform, ScrollView, Text, View } from 'react-native'
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
 
 export default function App() {
-  const [visibilityNotification, setVisibilityNotification] = useState(true);
+  // Panel Controller
+  const [vNotif, setVNotif] = useState(false);
+
+  // #1 Notifications and Special Notifications
+  const [expoPushToken, setExpoPushToken] = useState("");
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then((token) =>
+      setExpoPushToken(token)
+    );
+
+    notificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        setNotification(notification);
+      });
+
+    responseListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log(response);
+      });
+
+    return () => {
+      Notifications.removeNotificationSubscription(
+        notificationListener.current
+      );
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
 
   return (
-    <ScrollView>
+    <ScrollView style={{ padding: 40 }}>
+      {/* #1 Notifications and Special Notifications */}
       <Button
-        title="Notification"
-        onPress={() => {
-          setVisibilityNotification(!visibilityNotification);
-        }}
+        title="Notifications and Special Notifications"
+        onPress={() => setVNotif(!vNotif)}
       ></Button>
-      <View style={{ display: visibilityNotification ? "block" : "none" }}>
-        <Text>AWOKAWOK</Text>
+      <View
+        style={{
+          transform: [{ scale: vNotif ? 1 : 0 }],
+        }}
+      >
+        <Text>Your expo push token: {expoPushToken}</Text>
+        <View style={{ alignItems: "center", justifyContent: "center" }}>
+          <Text>
+            Title: {notification && notification.request.content.title}{" "}
+          </Text>
+          <Text>Body: {notification && notification.request.content.body}</Text>
+          <Text>
+            Data:{" "}
+            {notification && JSON.stringify(notification.request.content.data)}
+          </Text>
+        </View>
+        <Button
+          title="Press to schedule a notification"
+          onPress={async () => {
+            await schedulePushNotification();
+          }}
+        />
       </View>
-      <Button title="awokawok"></Button>
     </ScrollView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+// #1 Notifications and Special Notifications
+async function schedulePushNotification() {
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title: "You've got mail! 📬",
+      body: "Here is the notification body",
+      data: { data: "goes here" },
+    },
+    trigger: { seconds: 2 },
+  });
+}
+
+async function registerForPushNotificationsAsync() {
+  let token;
+  if (Constants.isDevice) {
+    const { status: existingStatus } =
+      await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== "granted") {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== "granted") {
+      alert("Failed to get push token for push notification!");
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    console.log(token);
+  } else {
+    alert("Must use physical device for Push Notifications");
+  }
+
+  if (Platform.OS === "android") {
+    Notifications.setNotificationChannelAsync("default", {
+      name: "default",
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: "#FF231F7C",
+    });
+  }
+
+  return token;
+}
