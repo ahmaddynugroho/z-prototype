117f25989168dc58952959e6103c9b5a055b4363
Ahmad D. Y. Nugroho <ahmaddwi700@gmail.com>
2021-12-28 01:00:49 +0700

    protoype: display none with button

--- App.js
+++ App.js
@@ -1,21 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react'
+import { Button, ScrollView, StyleSheet, Text, View } from 'react-native'
 
 export default function App() {
+  const [visibilityNotification, setVisibilityNotification] = useState(true);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView>
+      <Button
+        title="Notification"
+        onPress={() => {
+          setVisibilityNotification(!visibilityNotification);
+        }}
+      ></Button>
+      <View style={{ display: visibilityNotification ? "block" : "none" }}>
+        <Text>AWOKAWOK</Text>
+      </View>
+      <Button title="awokawok"></Button>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
